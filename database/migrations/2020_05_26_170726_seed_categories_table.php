3a41a410b99fcc4640c7bcb12f85af96602accda
woojuan <woojuan163@163.com>
2020-05-26 17:36:53 +0800

    新增代码生成器拓展

---
+++ database/migrations/2020_05_26_170726_seed_categories_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SeedCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        $categories = [
+            [
+                'name' => '日常',
+                'description' => '分享日常生活趣事'
+            ],
+            [
+                'name' => '教程',
+                'description' => '玩机教程'
+            ],
+            [
+                'name' => '问答',
+                'description' => '不懂的请在这里提问'
+            ],
+            [
+                'name' => '公告',
+                'description' => '站点公告'
+            ],
+
+        ];
+
+        //插入数据
+        DB::table('categories')->insert($categories);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //清空表,序号都清掉,重头开始
+        DB::table('categories')->truncate();
+
+    }
+}
