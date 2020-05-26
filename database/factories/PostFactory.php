6a4d9c82405ae35b502a5ac4b1e7323160afe6a0
woojuan <woojuan163@163.com>
2020-05-26 18:40:13 +0800

    填充用户和帖子数据

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,7 +4,16 @@ use Faker\Generator as Faker;
 
 
 $factory->define(App\Models\Post::class, function (Faker $faker) {
+
+    $updated_at = $faker->dateTimeThisMonth();//这个月内的随机时间
+    $created_at = $faker->dateTimeThisMonth($updated_at);//更改比生成要晚
+
     return [
         // 'name' => $faker->name,
+        'title' => $faker->sentence,
+        'body' => $faker->text,
+
+        'created_at' => $created_at,
+        'updated_at' => $updated_at
     ];
 });
--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -2,15 +2,19 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 
 class PostsTableSeeder extends Seeder
 {
     public function run()
     {
-        $posts = factory(Post::class)->times(50)->make()->each(function ($post, $index) {
-            if ($index == 0) {
-                // $post->field = 'value';
-            }
+        $faker = app(Faker\Generator::class);
+        $user_ids = User::all()->pluck('id')->toArray();//user_id 数组
+        $category_ids = Category::all()->pluck('id')->toArray();//category_id 数组
+        $posts = factory(Post::class)->times(50)->make()->each(function ($post, $index) use ($user_ids,$category_ids){
+            $post->user_id = array_rand($user_ids,1);
+            $post->category_id = array_rand($category_ids,1);
         });
 
         Post::insert($posts->toArray());
