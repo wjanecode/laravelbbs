07272ee9772fc93dc02fc9b445010c95c83d4ca3
woojuan <woojuan163@163.com>
2020-05-26 11:39:28 +0800

    登录改为用户名和邮箱都可以登录

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,41 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    //laravel默认使用邮箱加密码登录
+    //改成用户名/邮箱/手机号登录等
+    /**
+     * 重写方法
+     */
+    protected function attemptLogin(Request $request)
+    {
+
+        //
+        return collect(['name', 'email'])->contains(function ($value) use ($request) {
+            $account = $request->get($this->username());
+            $password = $request->get('password');
+            return $this->guard()->attempt([$value => $account, 'password' => $password], $request->filled('remember'));
+        });
+    }
+
+    protected function validateLogin(Request $request)
+    {
+        $this->validate($request, [
+            $this->username() => 'required|string',
+            'password' => 'required|string',
+
+        ],[
+            'captcha.required' => ':attribute 不能为空',
+
+        ],[
+            $this->username() => '账号',
+
+        ]);
+    }
+
+    public function username()
+    {
+        return 'account';
     }
 }
