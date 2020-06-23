5cca0c310b8f84ef7ff4551070adf2cef244fa7b
woojuan <woojuan163@163.com>
2020-06-23 16:00:37 +0800

    新回复通知,使用数据库方式

---
+++ app/Notifications/PostReplied.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Post;
+use App\Models\Reply;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class PostReplied extends Notification
+{
+    use Queueable;
+
+    protected $reply;//注入一个回复实体,方便使用
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Reply $reply)
+    {
+        //
+        $this->reply = $reply;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+
+    public function toDatabase( $notifiable ) {
+
+        //用数据库来通知数组会转成json格式保存到data字段
+        $post = $this->reply->post;
+        $link = route('posts.show',[$post->id,'replies'=>$this->reply->id]);
+
+        return [
+            'reply_id' => $this->reply->id,
+            'reply_content' => $this->reply->content,
+            'reply_created_at' => $this->reply->created_at,
+            'user_id' => $this->reply->user_id,
+            'user_avatar' => $this->reply->user->avatar,
+            'post_id' => $post->id,
+            'post_title' => $post->title,
+            'link' => $link
+        ];
+    }
+}
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,12 +3,25 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\PostReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class ReplyObserver
 {
+    public function created(Reply $reply  ) {
+        //帖子重新统计回复数
+        $reply->post->updateRepliesCount();
+
+        //向作者发送回复通知
+        $user = $reply->post->user;
+        $user->notify(new PostReplied($reply));
+        //通知数量加一
+        $user->increment('notification_count');
+        $user->save();
+
+    }
 
     public function saving(Reply $reply)
     {
@@ -17,11 +30,7 @@ class ReplyObserver
 
     }
 
-    public function saved(Reply $reply) {
-        //帖子重新统计回复数
-        $reply->post->updateRepliesCount();
 
-    }
 
     public function deleted(Reply $reply) {
 
